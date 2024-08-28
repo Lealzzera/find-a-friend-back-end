462cdf9d6cafda5b2c4aa950406629b2e09f2bc8
Matheus <matheus.mleall@gmail.com>
2024-08-28 19:33:45 -0300

    added pets repository interface new method

--- src/repositories/pets-repository.interface.ts
+++ src/repositories/pets-repository.interface.ts
@@ -2,4 +2,23 @@ import { Pet, Prisma } from "@prisma/client";
 
 export interface PetsRepositoryInterface {
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>;
+  findPetsByItsCharacteristics({
+    name,
+    breed,
+    type,
+    description,
+    age,
+    color,
+    size,
+    city,
+  }: {
+    name?: string;
+    breed?: string;
+    type?: string;
+    description?: string;
+    age?: string;
+    color?: string;
+    size?: "SMALL" | "MEDIUM" | "LARGE";
+    city: string;
+  }): Promise<Pet[] | null>;
 }
