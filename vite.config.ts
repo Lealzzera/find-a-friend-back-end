28bf22c7d70416f0856fd303d181b6276a5854de
Matheus <matheus.mleall@gmail.com>
2024-09-02 16:06:59 -0300

    added: exclude folders to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    coverage: {
+      exclude: [
+        "**/node_modules/**",
+        "./build",
+        "vite.config.ts",
+        "app.ts",
+        "server.ts",
+        "**/@types",
+      ],
+    },
     environmentMatchGlobs: [["src/http/controllers/**", "prisma"]],
     server: {
       deps: {
